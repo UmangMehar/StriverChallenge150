226061df0519059be630fd080e43540b8bc8733a
UmangMehar <umangmehar6@gmail.com>
2025-07-01 16:30:41 +0530

    Added solution for ReverseDigit

---
+++ src/basicmaths/ReverseDigits.java
@@ -0,0 +1,16 @@
+package basicmaths;
+
+public class ReverseDigits {
+    public static void main(String[] args) {
+        int n = 1234;
+        int reversed = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            reversed = reversed * 10 + digit;
+            n = n / 10;
+        }
+
+        System.out.println("Reversed number: " + reversed);
+    }
+}
