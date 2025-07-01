655eae29390a12b412a29f2179574bbce5cbd8c2
UmangMehar <umangmehar6@gmail.com>
2025-07-01 16:32:29 +0530

    Added solution for CheckPalindrome

---
+++ src/basicmaths/CheckPalindrome.java
@@ -0,0 +1,23 @@
+package basicmaths;
+
+public class CheckPalindrome {
+
+        public static void main(String[] args) {
+            int n = 121;
+            int original = n;
+            int reversed = 0;
+
+            while (n > 0) {
+                int digit = n % 10;
+                reversed = reversed * 10 + digit;
+                n = n / 10;
+            }
+
+            if (original == reversed) {
+                System.out.println(original + " is a palindrome");
+            } else {
+                System.out.println(original + " is not a palindrome");
+            }
+        }
+    }
+
