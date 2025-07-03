726205215b3401080d6a80178535829d2f609d70
UmangMehar <umangmehar6@gmail.com>
2025-07-03 19:30:32 +0530

    "Added solutions for Left Rotate by D Elements

---
+++ src/arrays/LeftRotateByD.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class LeftRotateByD {
+    public static void leftRotate(int[] arr, int d) {
+        int n = arr.length;
+        d = d % n;
+
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+
+    private static void reverse(int[] arr, int l, int r) {
+        while (l < r) {
+            int temp = arr[l];
+            arr[l++] = arr[r];
+            arr[r--] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int d = 3;
+        leftRotate(arr, d);
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+}
